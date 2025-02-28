c48edc8a444cb0a7c8eeb92be703a7816c8a5f23
Mohit Lokhande <mohit@Mohits-MacBook-Air.local>
2025-03-01 01:35:02 +0530

    Cases updated

--- cypress/e2e/LoginTest.cy.js
+++ cypress/e2e/LoginTest.cy.js
@@ -1,11 +1,12 @@
 describe("Login Tests", () => {
   beforeEach(function () {
-    cy.visit("/"); //To Visit the baseUrl defined in cypress.config.js
+    cy.visit("/"); // Visit the baseUrl defined in cypress.config.js
     cy.fixture("loginData").then((data) => {
       this.data = data;
     });
   });
 
+  // Positive test cases
   it("TC_01 - Login with valid credentials", function () {
     cy.get("#user-name").clear().type(this.data.validUser.username);
     cy.get("#password").clear().type(this.data.validUser.password);
@@ -41,64 +42,100 @@ describe("Login Tests", () => {
     cy.get("h3[data-test='error']").should("contain", "Username and password do not match");
   });
 
+  // Negative test cases
+  it("TC_05 - Login with incorrect username", function () {
+    cy.get("#user-name").clear().type(this.data.invalidUser.username);
+    cy.get("#password").clear().type(this.data.validUser.password);
+    cy.get("#login-button").click();
+    cy.get("h3[data-test='error']").should("contain", "Username and password do not match");
+  });
+
+  it("TC_06 - Login with incorrect password", function () {
+    cy.get("#user-name").clear().type(this.data.validUser.username);
+    cy.get("#password").clear().type(this.data.invalidUser.password);
+    cy.get("#login-button").click();
+    cy.get("h3[data-test='error']").should("contain", "Username and password do not match");
+  });
+
+  it("TC_07 - Login with empty fields", function () {
+    cy.get("#login-button").click();
+    cy.get("h3[data-test='error']").should("contain", "Username is required");
+  });
+
+  it("TC_08 - Login with only username filled", function () {
+    cy.get("#user-name").clear().type(this.data.validUser.username);
+    cy.get("#login-button").click();
+    cy.get("h3[data-test='error']").should("contain", "Password is required");
+  });
+
+  it("TC_09 - Login with only password filled", function () {
+    cy.get("#password").clear().type(this.data.validUser.password);
+    cy.get("#login-button").click();
+    cy.get("h3[data-test='error']").should("contain", "Username is required");
+  });
+
+  it("TC_10 - Login with spaces before/after username", function () {
+    cy.get("#user-name").clear().type(" standard_user ");
+    cy.get("#password").clear().type(this.data.validUser.password);
+    cy.get("#login-button").click();
+  });
+
+  it("TC_11 - Login with spaces before/after password", function () {
+    cy.get("#user-name").clear().type(this.data.validUser.username);
+    cy.get("#password").clear().type(" secret_sauce ");
+    cy.get("#login-button").click();
+  });
+
   it("TC_12 - Login as locked_out_user", function () {
-    cy.get("#user-name").clear().type(this.data.lockedOutUser.username);
+    cy.get("#user-name").clear().type("locked_out_user");
     cy.get("#password").clear().type(this.data.validUser.password);
     cy.get("#login-button").click();
     cy.get("h3[data-test='error']").should("contain", "Sorry, this user has been locked out.");
   });
 
+  // Special User Scenarios
   it("TC_13 - Login as performance_glitch_user", function () {
-    cy.get("#user-name").clear().type(this.data.performanceGlitchUser.username);
+    cy.get("#user-name").clear().type("performance_glitch_user");
     cy.get("#password").clear().type(this.data.validUser.password);
     cy.get("#login-button").click();
     cy.url().should("include", "/inventory.html");
   });
 
   it("TC_14 - Login as problem_user", function () {
-    cy.get("#user-name").clear().type(this.data.problemUser.username);
+    cy.get("#user-name").clear().type("problem_user");
     cy.get("#password").clear().type(this.data.validUser.password);
     cy.get("#login-button").click();
     cy.url().should("include", "/inventory.html");
   });
 
   it("TC_15 - Login as error_user", function () {
-    cy.get("#user-name").clear().type(this.data.errorUser.username);
+    cy.get("#user-name").clear().type("error_user");
     cy.get("#password").clear().type(this.data.validUser.password);
     cy.get("#login-button").click();
   });
 
-  it("TC_16 - Session Expiry after Login", function () {
+  // Edge Cases
+  it("TC_16 - Login with long username input", function () {
+    cy.get("#user-name").clear().type("a".repeat(100));
+    cy.get("#password").clear().type(this.data.validUser.password);
+    cy.get("#login-button").click();
+  });
+
+  it("TC_17 - Login with long password input", function () {
     cy.get("#user-name").clear().type(this.data.validUser.username);
-    cy.get("#password").clear().type(this.data.validUser.password);
+    cy.get("#password").clear().type("a".repeat(100));
     cy.get("#login-button").click();
-    cy.wait(9000); // Wait for 15 minutes
-    cy.reload();
-    cy.get("h3[data-test='error']").should("contain", "Session expired");
   });
 
-  it("TC_17 - Login with long username input", function () {
-    cy.get("#user-name").clear().type(this.data.longUsername);
+  it("TC_18 - Login with Special Characters in Username", function () {
+    cy.get("#user-name").clear().type("!@#$%^&*");
     cy.get("#password").clear().type(this.data.validUser.password);
     cy.get("#login-button").click();
   });
 
-  it("TC_18 - Login with long password input", function () {
+  it("TC_19 - Login with Special Characters in Password", function () {
     cy.get("#user-name").clear().type(this.data.validUser.username);
-    cy.get("#password").clear().type(this.data.longPassword);
-    cy.get("#login-button").click();
-  });
-
-  it("TC_19 - Login with Special Characters in Username", function () {
-    cy.get("#user-name").clear().type(this.data.specialChars.username);
-    cy.get("#password").clear().type(this.data.validUser.password);
-    cy.get("#login-button").click();
-    //hi
-  });
-
-  it("TC_20 - Login with Special Characters in Password", function () {
-    cy.get("#user-name").clear().type(this.data.validUser.username);
-    cy.get("#password").clear().type(this.data.specialChars.password);
+    cy.get("#password").clear().type("!@#$%^&*");
     cy.get("#login-button").click();
   });
 });
